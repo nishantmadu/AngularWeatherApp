b073e131f3f881921e478b2122a9b06ca3824c7e
nishantmadu <nishant.madu@gmail.com>
2016-06-05 15:28:33 +0530

    Final changes

--- app.js
+++ app.js
@@ -24,11 +24,22 @@ angularApp.controller("HomeController",function($resource){
     vm.weather.$promise.then(function(data){
       vm.weatherResponse = data;
       vm.weatherResponse.main.temp -= 273;
+      vm.weatherResponse.main.degree = Math.round(vm.weatherResponse.main.temp)
+      console.log(vm.weatherResponse.main.newDate);
       console.log(vm.weatherResponse)
+      console.log(vm.weatherResponse.main.dt);
     }, function(error){
       console.log(error);
     });
-   }
+  };
+
+   vm.convertDate = function(x){
+     return Date(x*1000);
+   };
+
+   vm.tingu = function(x){
+     return x;
+   };
 });
 
 angularApp.controller("ForecastController",function($resource){
@@ -38,7 +49,11 @@ angularApp.controller("ForecastController",function($resource){
     var forecastResource = $resource('http://api.openweathermap.org/data/2.5/forecast',
       {q:vm.cityName,appid:'27d43832d2a4adcb97fcbfa23db130aa',cnt:10});
     vm.forecastResponse = forecastResource.get();
+
     console.log(vm.forecastResponse);
    }
+   vm.newTemp = function(x){
+     return Math.round(x-273);
+   };
 
 });
